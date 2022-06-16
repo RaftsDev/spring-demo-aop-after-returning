23c6a8c9bd08190386ffd560742d2cf0d8269f87
Ralph <raftsdev@gmail.com>
2022-06-16 16:02:42 -0400

    Feat: Invoke methods in App

---
+++ src/com/luv2code/aopdemo/AfterReturningDemoApp.java
@@ -0,0 +1,39 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class AfterReturningDemoApp {
+
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		
+		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
+		
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		theAccountDAO.setName("FooBar");
+		theAccountDAO.setServiceCode("silver");
+		
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+		
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+		context.close();
+	}
+
+}
